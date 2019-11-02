d9667d154b7586562f620f6e2305f6e1b62713ee
jiayingy <jiayinglek@gmail.com>
2019-11-03 02:21:10 +0800

    Create calendar header component

---
+++ src/components/CalendarHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <th>
+    <p>{{ day }}</p>
+  </th>
+</template>
+
+<script>
+export default {
+  props: {
+    day: {
+      type: String,
+      default: () => ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
