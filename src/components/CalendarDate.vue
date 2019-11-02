94983b84e838bc18cc943a5c5c748c4cb8c58354
jiayingy <jiayinglek@gmail.com>
2019-11-03 02:23:42 +0800

    Create child components for dates

---
+++ src/components/CalendarDate.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="date">
+    {{ date }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    date: {
+      type: Number,
+      default: () => 1
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.date {
+  height: 10px;
+  width: 10px;
+  font-size: 10px;
+}
+</style>
---
+++ src/components/CalendarWeek.vue
@@ -0,0 +1,28 @@
+<template>
+  <tr>
+    <CalendarDate
+      v-for="date in dates"
+      :key="date"
+      :date="date"
+    />
+  </tr>
+</template>
+
+<script>
+import CalendarDate from './CalendarDate';
+export default {
+  components: {
+    CalendarDate
+  },
+  props: {
+    dates: {
+      type: Number,
+      default: () => 30
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
