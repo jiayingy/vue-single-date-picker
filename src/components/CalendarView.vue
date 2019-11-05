88b4ba91f0ace04930b90e72c977c4e3d87c1ad0
jiayingy <jiayinglek@gmail.com>
2019-11-05 21:02:35 +0800

    Add styles to calendar table

--- src/components/CalendarView.vue
+++ src/components/CalendarView.vue
@@ -93,6 +93,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+table {
+  padding: 15px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 0 10px 3px rgba(0, 0, 0, 0.1);
+}
 </style>
