d54cf7901a953761c8d6c870cb6d83123b2c8b03
jiayingy <jiayinglek@gmail.com>
2019-11-20 01:29:32 +0800

    Diplay border on dates on hover

--- src/components/CalendarDate.vue
+++ src/components/CalendarDate.vue
@@ -22,10 +22,18 @@ export default {
 
 <style lang="scss" scoped>
 .date {
-  height: 10px;
-  width: 10px;
   font-size: 10px;
-  padding: 10px;
   font-size: 14px;
+  height: 30px;
+  width: 30px;
+  display: table-cell;
+  vertical-align: middle;
+  box-sizing: border-box;
+  cursor: default;
+
+  &:hover {
+    border-radius: 50%;
+    border: 2px solid pink;
+  }
 }
 </style>
