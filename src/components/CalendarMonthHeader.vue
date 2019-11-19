e32c378675001cd4aa53b1c6c8ab293078a174b7
jiayingy <jiayinglek@gmail.com>
2019-11-20 01:54:33 +0800

    Display arrows to toggle month

--- src/components/CalendarMonthHeader.vue
+++ src/components/CalendarMonthHeader.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="calendar-month-header">
-    {{ fullMonth }} {{ year }}
+    <div class="arrow left">
+      <i class="material-icons">
+        keyboard_arrow_left
+      </i>
+    </div>
+    <div class="year">
+      {{ fullMonth }} {{ year }}
+    </div>
+    <div class="arrow right">
+      <i class="material-icons">
+        keyboard_arrow_right
+      </i>
+    </div>
   </div>
 </template>
 
@@ -41,11 +53,19 @@ export default {
 <style lang="scss" scoped>
 .calendar-month-header {
   padding: 15px;
-  font-weight: 900;
-  font-size: 20px;
-  padding: 15px;
-  border-radius: 10px 10px 0 0;
-  text-transform: capitalize;
   background-image: linear-gradient(to right, #355C7D, #6C5B7B, #C06C84); 
+  display: flex;
+  justify-content: space-between;
+  border-radius: 10px 10px 0 0;
+  align-items: center;
+
+  .year {
+    font-weight: 900;
+    font-size: 20px;
+    text-transform: capitalize;
+  }
+  .arrow {
+    cursor: pointer;
+  }
 }
 </style>
