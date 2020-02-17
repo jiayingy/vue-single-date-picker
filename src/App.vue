0da828cd4c0f8e335e1b2681a31d90ba204ce2ef
jiayingy <jiayinglek@gmail.com>
2020-02-18 01:42:14 +0800

    Create separate entry point for demo webpage

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
+    <h1>single date picker</h1>
     <calendar-view />
   </div>
 </template>
 
 <script>
-import CalendarView from './components/CalendarView.vue'
+import CalendarView from './CalendarView.vue'
 
 export default {
   name: 'App',
@@ -15,13 +16,27 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  color: #0d1e44;
+  margin: auto;
+  padding: 30px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+#single-date-picker {
+  margin: auto;
+}
+
+h1 {
+  color: #c7a3b5;
+  background: linear-gradient(to right, #e0caca, #d4b8ca, #c7a3b5);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 60px;
+  font-weight: 300;
+  font-family: 'Lato', Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+  letter-spacing: 1px;
 }
 </style>
